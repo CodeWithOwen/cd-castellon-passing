1e09044ece8884e01f0eea1a1297ba170d1727cd
Owen Tuleja <otuleja@Owens-MacBook-Pro.local>
2024-04-26 22:01:45 +0200

    with type script

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing deploy!
+          Testing deploy with type script!
         </p>
         <a
           className="App-link"
